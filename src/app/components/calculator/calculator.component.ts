74c02cd8dc51168aacf01b060cece5826faa5d20
Goli Akshay <goliakshay357@gmail.com>
2021-03-12 01:23:53 +0530

    Added calculator functionality

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -8,8 +8,49 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   constructor() { }
+  answer:any;
 
   ngOnInit(): void {
   }
 
+  calculate(){
+    //Getting the number
+    let temp:any = document.getElementById("number");
+    let number = parseInt(temp.value);
+    console.log(typeof number);
+
+    //Getting the selector
+    let e:any = document.getElementById("sel1");
+    let selectValue = e.value;
+    console.log(selectValue);
+   
+    // Sqrt
+    if(selectValue === "1"){
+       this.answer=Math.sqrt(number); 
+    }
+
+    // Factorial
+    if(selectValue === "2"){
+      this.answer= this.sFact(number); 
+    }
+    
+    // Natural Logerithm
+    if(selectValue === "3"){
+      this.answer= Math.log(number); 
+    }
+    
+    // Power function
+    if(selectValue === "4"){
+      this.answer= Math.pow(number, 3); 
+    }
+    
+  }
+
+  sFact(num:number)
+  {
+    var rval=1;
+    for (var i = 2; i <= num; i++)
+        rval = rval * i;
+    return rval;
+  }
 }
