3d914268c5e8e4d5aa3f60a7096b2201d1b69e0b
goliakshay357 <goliakshay357@gmail.com>
2021-03-16 23:36:28 +0530

    Testing secrets in entrypoint.sh

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -2,4 +2,9 @@
 
 echo "Ansible Entrypoint"
 
+echo "This is the secret: $SSH_PASSWORD"
+
+echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
+echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
+
 ansible-playbook ansible/playbook.yml --user ubuntu
